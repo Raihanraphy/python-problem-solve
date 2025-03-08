1646b3222d5385d77762cfb94dd08b9acf249867
Raihan Rahman <raihanraphy@gmail.com>
2025-03-09 03:50:29 +0600

    Create built_in.py

---
+++ built_in.py
@@ -0,0 +1,16 @@
+s = input()
+upper, lower,odd, even  = [], [], [], []
+
+for char in s:
+    if char.isnumeric():
+        if int(char)%2 == 0:
+            even.append(char)
+        else:
+            odd.append(char)
+    else:
+        if char.isupper():
+            upper.append(char)
+        else:
+            lower.append(char)
+
+print(''.join(sorted(lower))+''.join(sorted(upper))+''.join(sorted(odd))+''.join(sorted(even)))
