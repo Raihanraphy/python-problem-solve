b994609077547cc0ebb64212cd7dd14765e67263
Raihan Rahman <raihanraphy@gmail.com>
2025-07-14 23:44:16 +0600

    Create random.py

---
+++ random.py
@@ -0,0 +1,10 @@
+import random
+quotes = [
+    "Code is poetry.",
+    "Hello, World!",
+    "Keep it simple.",
+    "Innovate.",
+    "Learn by doing.",
+    "Open source rocks!"
+]
+print(random.choice(quotes))
