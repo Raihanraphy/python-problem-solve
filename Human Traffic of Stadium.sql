9e9651da0f43e6ab1e039c9ac4fa17a7be61ccab
Raihan Rahman <raihanraphy@gmail.com>
2025-07-15 00:49:35 +0600

    Create Human Traffic of Stadium.sql

---
+++ Human Traffic of Stadium.sql
@@ -0,0 +1,17 @@
+WITH res_cte AS (
+SELECT 
+    *,
+    id - dense_rank() OVER (ORDER BY visit_date) level
+FROM
+    Stadium
+WHERE people >= 100)
+,final_cte AS (
+SELECT 
+    id,
+    visit_date,
+    people,
+    COUNT(*) OVER (PARTITION BY level) consec_count
+    FROM res_cte
+)
+
+SELECT id, visit_date, people FROM final_cte WHERE consec_count > 2 ORDER BY visit_date;
