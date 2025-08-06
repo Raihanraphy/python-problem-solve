f60fbdcf9ce69fcd7ad324b88cc69556adf70342
Raihan Rahman <raihanraphy@gmail.com>
2025-08-06 07:29:06 +0600

    Update gray-code.py

--- gray-code.py
+++ gray-code.py
@@ -1,1 +1,7 @@
-gray-code
+#gray-code
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        if n == 0: return [0]
+        prev = self.grayCode(n-1)  # Get (n-1)-bit sequence
+        add = 1 << (n-1)  # 2^(n-1) for prefixing
+        return prev + [x + add for x in prev[::-1]]  # Append reversed with 1-prefix
