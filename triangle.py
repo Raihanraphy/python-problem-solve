50024978253344fe995e7ed7a0041cc75ceb2eff
Raihan Rahman <raihanraphy@gmail.com>
2025-09-14 07:03:20 +0600

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        row = len(triangle)
+        dp = [0] * row
+        dp[0] = triangle[0][0]
+        for i in range(1, row):
+            dp[i] = dp[i-1] + triangle[i][i]
+            for j in range(i-1, 0, -1):
+                dp[j] = min(dp[j], dp[j-1]) + triangle[i][j]
+            dp[0] = dp[0] + triangle[i][0]
+        return min(dp)
